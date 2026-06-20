9fcab285e69f1a5fc8a1ba50974d398c9e21d9b5
Carly <77548696+carly922@users.noreply.github.com>
2026-06-20 15:05:10 -0400

    trying to fix wheel

--- sisyphus-simulator/ui_wheel.gd
+++ sisyphus-simulator/ui_wheel.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var deadzone: float = 100.0
+
 var mouse_hover = false
 var is_dragging = false
 var prev_mouse_angle
@@ -8,21 +8,21 @@ var wheel_rotate_amount: float = 0
 
 func _ready() -> void:
 	Input.use_accumulated_input = false
+	set_process_input(true)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		mouse_hover = true
 		prev_mouse_angle = (get_global_mouse_position() - global_position).angle()
-		is_dragging = event.velocity
+		is_dragging = event
 	else:
 		mouse_hover = false
 
-func _physics_process(_delta: float) -> void:
+func _process(_delta: float) -> void:
 	if is_dragging:
 		var mouse_velocity = Input.get_last_mouse_velocity()
 		var mouse_pos = get_global_mouse_position()
-		if mouse_pos.distance_to(global_position) >= deadzone:
-			var mouse_angle = (mouse_pos - global_position).angle()
-			var angle_delta = angle_difference(prev_mouse_angle, mouse_angle)
-			rotation += angle_delta
-			prev_mouse_angle = mouse_angle
+		var mouse_angle = (mouse_pos - $"../../CenterScreen".position).angle()
+		var angle_delta = angle_difference(prev_mouse_angle, mouse_angle)
+		rotation += angle_delta
+		prev_mouse_angle = mouse_angle
