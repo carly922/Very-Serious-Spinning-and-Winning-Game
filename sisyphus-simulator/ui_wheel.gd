8738fb637007c795bb8d73fb42f90dbccfe54373
Carly <77548696+carly922@users.noreply.github.com>
2026-06-20 13:35:47 -0400

    breaking the wheel system
    - trying to get it so velocity is at play and so you dont need to make gigantic circles for it to register mouse movement
    - have not fixed any of these issues yet

--- sisyphus-simulator/ui_wheel.gd
+++ sisyphus-simulator/ui_wheel.gd
@@ -1,27 +1,27 @@
 extends Area2D
 
-@export var deadzone: float = 30.0
+@export var deadzone: float = 100.0
 var mouse_hover = false
 var is_dragging = false
 var prev_mouse_angle
 var wheel_rotate_amount: float = 0
 
-func _on_mouse_entered() -> void:
-	mouse_hover = true
-
-func _on_mouse_exited() -> void:
-	mouse_hover = false
+func _ready() -> void:
+	Input.use_accumulated_input = false
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			prev_mouse_angle = (get_global_mouse_position() - global_position).angle()
-			is_dragging = event.pressed
+	if event is InputEventMouseMotion:
+		mouse_hover = true
+		prev_mouse_angle = (get_global_mouse_position() - global_position).angle()
+		is_dragging = event.velocity
+	else:
+		mouse_hover = false
 
 func _physics_process(_delta: float) -> void:
 	if is_dragging:
+		var mouse_velocity = Input.get_last_mouse_velocity()
 		var mouse_pos = get_global_mouse_position()
-		if mouse_pos.distance_to(global_position) > deadzone:
+		if mouse_pos.distance_to(global_position) >= deadzone:
 			var mouse_angle = (mouse_pos - global_position).angle()
 			var angle_delta = angle_difference(prev_mouse_angle, mouse_angle)
 			rotation += angle_delta
