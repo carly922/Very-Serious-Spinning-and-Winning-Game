f04eaf2261ded0788d0d5996e4a7b632ccfcb02a
Waffle <64041836+wafflemaker-pro@users.noreply.github.com>
2026-06-22 21:52:37 -0400

    okay chunks didnt work before
    and they still dont but less so now

--- sisyphus-simulator/chunk_1.gd
+++ sisyphus-simulator/chunk_1.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
+signal chunk_death
 
 func _on_screen_notifier_screen_exited() -> void:
 	print("Successful Death!")
+	chunk_death.emit()
 	queue_free()
+
+func _physics_process(delta: float) -> void:
+	position.x -= -400 * delta
+	position.y -= -200 * delta
--- sisyphus-simulator/chunk_manager.gd
+++ sisyphus-simulator/chunk_manager.gd
@@ -1,18 +1,20 @@
 extends Node2D
 
 @export var chunk1: PackedScene
+@export var chunk1_2: PackedScene
 var local_chunk1
-var chunk_x = 1152
-var chunk_y = 648
+var chunk_x = -1232
+var chunk_y = -617
 
 func _ready() -> void:
 	local_chunk1 = chunk1.instantiate()
 	add_child(local_chunk1)
 
 func _physics_process(delta: float) -> void:
-	local_chunk1.position.x -= -400 * delta
-	local_chunk1.position.y -= -200 * delta
 	var next_chunk1 = chunk1.instantiate()
-	next_chunk1.position = Vector2(chunk_x, chunk_y)
+	next_chunk1.global_position = Vector2(chunk_x, chunk_y)
 	add_child(next_chunk1)
+	await local_chunk1.chunk_death
 	local_chunk1 = next_chunk1
+	return
+	
